9730845124862931269d487ca58421840efd5425
Eric Normand <ericwnormand@gmail.com>
2016-08-12 09:30:26 -0500

    Add simple request.

--- src/clj_http_playground/core.clj
+++ src/clj_http_playground/core.clj
@@ -1,2 +1,10 @@
-(ns clj-http-playground.core)
+(ns clj-http-playground.core
+  (:require [clj-http.client :as http]))
+
+(def response (http/get "http://lispcast.com"
+                {:debug? true}))
+
+(:status response)
+(:server (:headers response))
+(type (:body response))
 
